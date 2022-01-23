57c3c7b352ec5ccf18e0a8ccdbeea6a3a86fefa8
Mateusz Wittek <mati128xd@gmail.com>
2022-01-23 16:41:52 +0100

    tailwind theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@ module.exports = {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
+    fontFamily: {
+      'sans': ['Lato', 'sans-serif']
+    },
+    colors: {
+      'base': '#3F66A2',
+      'heading': '#0E0C11',
+      'background': '#FAFCFE',
+      'font-dark-base': '#AAAAAA',
+      'font-light-base': '#FFF',
+      'font-light-secondary': '#79797A',
+      'gray': '#D1D1D1'
+    },
+    borderRadius: {
+      DEFAULT: '15px'
+    },
     extend: {},
   },
   plugins: [],
